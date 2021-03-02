f5bd20b5d2c08d3becb45740133912ca2f56c77e
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-03-02 18:32:50 -0300

    Add public.css | Styles | Template

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set('view options',{delimiter:"%"});
 app.use(bodyParser.urlencoded({extended:true}));
+var tasks = [];
 
 app.get("/", (req,res)=>{
-    res.render("hello",{text:["Greetings","stranger "].join(", ")});
+    let today = new Date();
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    }
+    res.render("todo",{
+        day: today.toLocaleDateString("pt-BR",options),
+        tasks: tasks
+    });
+});
+
+app.post("/",(req,res)=>{
+
+    if(req.body.clear){
+        tasks=[];
+    }else{
+        tasks.push(req.body.newTask);
+    }
+   res.redirect("/");
 });
 
 app.listen(3000,()=>{
     console.log("Listening on http://localhost:3000");
-})
+});
