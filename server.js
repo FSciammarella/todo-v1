f485461eb7e20943009c14ece9c0dcf6d07dfd22
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-03-02 18:36:09 -0300

    Adjusts por to use process.env.PORT

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post("/",(req,res)=>{
    res.redirect("/");
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Listening on http://localhost:3000");
 });
