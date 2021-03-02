4e8889b77963e1c838327786775a1c1e8d964fb9
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-03-02 18:51:42 -0300

    Removes left margin of forms | fixes console log of PORT

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post("/",(req,res)=>{
 });
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log("Listening on http://localhost:3000");
+    console.log(`Listening on http://localhost:${process.env.PORT || 3000}`);
 });
