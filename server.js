80550fa914e743a809033671a952b6594957d307
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-03-06 21:51:13 -0300

    refactored for readability

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
+/**
+ * @file Simple express.js to-do backend with Mongo Atlas integration through Mongoose based on Angela Yu's AppBrewery Bootcamp
+ * @author Felipe Sciammarella <felipe.sciammarella@gmail.com>
+ * @copyright Felipe Sciammarella 2021
+ * @license ISC
+ */
+
+/*
+ * Utilities
+ */
 require("dotenv").config();
 const _ = require("lodash");
 const dayjs = require("dayjs");
 
+/*
+ * Express configuration
+ */
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set('view options', {
+    delimiter: "%"
+});
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/*
+ * Mongoose Configuration
+ */
+
 const mongoose = require("mongoose");
-const {
-    result
-} = require("lodash");
-const {
-    query
-} = require("express");
+
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
+
+/*
+ * Data Schema definitions
+ */
 
 const tasksSchema = new mongoose.Schema({
     description: {
@@ -26,6 +53,7 @@ const tasksSchema = new mongoose.Schema({
     },
     done: Boolean
 });
+
 const listsSchema = new mongoose.Schema({
     tasks: [tasksSchema],
     date: {
@@ -40,60 +68,72 @@ const listsSchema = new mongoose.Schema({
 });
 
 const List = new mongoose.model("List", listsSchema);
-// const Task = new mongoose.model("Task", tasksSchema);
 
+/*
+ * Function declarations
+ */
 
-const app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set('view options', {
-    delimiter: "%"
-});
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+function deleteTask(taskList, _id, cb = () => {}) {
+    /** 
+     * @summary Removes a task from the TaskList document and optionally chains a callback
+     * @param TaskList taskList - Tasklist document from which the task will be removed.
+     * @param ObjectID _id - task subdocument id.
+     */
 
+    let to_delete = taskList.tasks.id(_id);
+    if (to_delete) {
+        to_delete.remove();
+    }
+    taskList.save().then(cb);
+}
 
-// const task = [
-//     new Task({
-//         description:"Do nothing",
-//         done:false
-//     }),
-//     new Task({
-//         description:"Buy Food",
-//         done:false
-//     }),
-//     new Task({
-//         description:"Sleep",
-//         done:true
-//     })
-// ]
+function updateDoneTasks(taskList, _ids) {
+    /** 
+     * @summary udpate done Tasks from a tasklist 
+     * @param TaskList taskList - Tasklist document from which the task will be updated.
+     * @param ObjectID _ids - subdocument ids of tasks marked as done.
+     */
+    for (task of taskList.tasks) {
+        if (_ids.includes(task._id.toString())) {
+            task.done = true;
+        } else {
+            task.done = false;
+        }
+    }
+}
 
-// const tasklist = new List({
-//     name:dayjs().format("DD-MM-YYYY"),
-//     tasks:[]
-// });
-
-// tasklist.tasks.push(...task);
-
-// tasklist.save();
-
+/*
+ * Endpoints configuration
+ */
 
 app.get("/", (req, res) => {
+
     let query = dayjs().format("DD-MM-YYYY");
-    List.find({
+    List.findOne({
         name: query
     }, (err, result) => {
-        res.render("todo", {
-            list: result[0]
-        })
+        if (!result) {
+            result = new List({
+                name: query
+            });
+        }
+        result.save().then(
+            () => {
+                res.render("todo", {
+                    list: result
+                })
+            }
+
+        ).catch((err) => {
+            console.error(err)
+        });
     });
 
 });
 
 app.get("/:listName", (req, res) => {
     let query = req.params.listName;
-    if (query == "favicon.ico"){
+    if (query == "favicon.ico") {
         res.sendStatus(404);
     };
 
@@ -107,6 +147,8 @@ app.get("/:listName", (req, res) => {
             })
             newList.save().then(() => {
                 res.redirect("/" + req.params.listName);
+            }).catch((err) => {
+                console.error(err)
             });
         } else {
             res.render("todo", {
@@ -123,59 +165,49 @@ app.post("/:listName", (req, res) => {
     List.findOne({
         name: req.params.listName
     }, (err, result) => {
-        if(req.body.delete){
-            let to_delete = result.tasks.id(req.body.delete);
-            if (to_delete){
-                to_delete.remove();
-            }
-            result.save().then(()=>{
-                res.redirect("/"+req.params.listName);
-            });
+
+        const redirect = () => {
+            res.redirect("/" + req.params.listName)
+        }
+
+        if (req.body.delete) {
+            deleteTask(result, req.body.delete, redirect)
             return;
         }
 
-        req.body.id = req.body.id || [];
-        for (task of result.tasks){
-            console.log(task._id);
-            if (req.body.id.includes(task._id.toString())){
-                console.log("worked");
-                task.done = true;
-            }else{
-                task.done = false;
-            }
+        if (req.body.id) {
+            updateDoneTasks(result, req.body.id);
         }
+
         if (req.body.newTask) {
             task = {
                 description: req.body.newTask,
                 done: false
             };
             result.tasks.push(task);
-
         };
-        result.save().then(() => {
-            res.redirect("/" + req.params.listName)
+        result.save().then(redirect).catch((err) => {
+            console.error(err)
         });
     })
 });
 
 
-app.post("/:listName/clear",(req,res)=>{
+app.post("/:listName/clear", (req, res) => {
+    /*
+     * "Hidden" route that clears all saved tasks
+     */
     List.update({
         "tasks.done": true
-    },{
-        "$pull":{
-            "tasks":{
-                "done":true
+    }, {
+        "$pull": {
+            "tasks": {
+                "done": true
             }
         }
-    }, function(err, result){
-        console.error(err);
-        console.log(result);
-    }).then(
-    ()=>{
-        res.redirect("/"+req.params.listName)
-    }        
-    )
+    }).then(() => {
+        res.redirect("/" + req.params.listName)
+    }).catch((err)=>{console.error(err)});
 })
 
 
